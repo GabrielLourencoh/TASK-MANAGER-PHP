f5a659ebc82ad23a74555b6f587eb25d45d2623b
GabrielLourencoh <gl930551@gmail.com>
2025-10-23 15:42:48 -0300

    refactor(database): rename fields and add status column in task table

--- task_manager.sql
+++ task_manager.sql
@@ -1,5 +1,25 @@
-CREATE DATABASE IF NOT EXISTS `task_manager` DEFAULT CHARACTER SET tis620 COLLATE tis620_bin;
-USE `task_manager`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23/10/2025 às 15:52
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `task_manager`
+--
 
 -- --------------------------------------------------------
 
@@ -9,11 +29,23 @@ USE `task_manager`;
 
 CREATE TABLE `task` (
   `ID` int(11) NOT NULL,
-  `DATETIME` datetime NOT NULL,
-  `TITLE` varchar(150) NOT NULL,
-  `DESCRIPTION` longtext NOT NULL,
-  `USERID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=tis620 COLLATE=tis620_bin;
+  `DATE_TIME` datetime NOT NULL,
+  `TITLE` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `DESCRIPTION` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `STATUS` int(11) NOT NULL,
+  `USER_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `task`
+--
+
+INSERT INTO `task` (`ID`, `DATE_TIME`, `TITLE`, `DESCRIPTION`, `STATUS`, `USER_ID`) VALUES
+(1, '2025-10-23 10:42:00', 'titulo da task', 'task criada', 1, 3),
+(2, '2025-10-23 10:52:16', 'titulo da task', 'task criada', 1, 3),
+(3, '2025-10-23 10:52:18', 'titulo da task', 'task criada', 1, 3),
+(4, '2025-10-23 10:52:18', 'titulo da task', 'task criada', 1, 3),
+(5, '2025-10-23 10:52:19', 'titulo da task', 'task criada', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -28,6 +60,16 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `level` int(11) NOT NULL COMMENT '1 - ADMIN | 2 - USER'
 ) ENGINE=InnoDB DEFAULT CHARSET=tis620 COLLATE=tis620_bin;
+
+--
+-- Despejando dados para a tabela `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `level`) VALUES
+(1, 'Gabriel Lourenco', 'lourenco@teste.com', 'minhasenha123', 1),
+(2, 'Gabriel Lourenco', 'lourenco@teste.com', 'minhasenha123', 1),
+(3, 'Gabriel Lourenco', 'lourenco@teste.com', 'minhasenha123', 1),
+(4, 'Gabriel Lourenco', 'lourenco@teste.com', 'minhasenha123', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -53,23 +95,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de tabela `task`
 --
 ALTER TABLE `task`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Banco de dados: `task_manager_bd`
---
-CREATE DATABASE IF NOT EXISTS `task_manager_bd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `task_manager_bd`;
---
--- Banco de dados: `test`
---
-CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `test`;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
